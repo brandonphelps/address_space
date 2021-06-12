c26cb7db8461fd27bd144c3057b50506a66b67d5
brandon phelps <bwp44f@mst.edu>
2021-06-12 16:31:31 -0500

    added in address space definition

--- src/address_space.rs
+++ src/address_space.rs
@@ -119,7 +119,125 @@ fn merge_sections(sec1: &Section, sec2: &Section) -> Option<Section> {
 }
 
 
+pub struct AddressSpace {
+    data: Vec<Section>,
+}
 
+impl AddressSpace {
+    pub fn new() -> Self {
+        Self {
+            data: Vec::new(),
+        }
+    }
+
+    // todo: can we merge these and the second one?
+    fn find_section(&self, addr: u32) -> Option<&Section> {
+        for i in self.data.iter() {
+            if i.start_addr < addr {
+                if addr < i.end_addr() {
+                    return Some(i);
+                }
+            } else if i.start_addr == addr {
+                return Some(i);
+            }
+        }
+        return None;
+    }
+
+    fn find_section_mut(&mut self, addr: u32) -> Option<&mut Section> {
+        for i in self.data.iter_mut() {
+            if i.start_addr < addr {
+                if addr < i.end_addr() {
+                    return Some(i);
+                }
+            } else if i.start_addr == addr {
+                return Some(i);
+            } else if i.start_addr > addr {
+                return None;
+            }
+        }
+        return None;
+    }
+
+    fn find_neighboring_section(&self, sec: &Section) -> Option<&Section> {
+        for i in self.data.iter() {
+            if sec.end_addr() == i.start_addr {
+                return Some(i);
+            }
+        }
+        return None;
+    }
+
+    /// @breif searches for neighbors and joins them together.
+    /// will only perform a single neighbor search and join.
+    fn consolidate(&mut self) {
+        let mut has_neighbor_index = None;
+        for (index, i) in self.data.iter().enumerate() {
+            let p = self.find_neighboring_section(i);
+            if p.is_some() {
+                has_neighbor_index = Some(index + 1);
+                break;
+            }
+        }
+
+        match has_neighbor_index {
+            Some(index) => {
+                let neighbor = self.data.swap_remove(index);
+                self.data[index - 1].merge_with(neighbor);
+            }
+            None => {}
+        }
+    }
+
+    // maintains sorted order via insertion sort.
+    fn insert_section(&mut self, section: Section) {
+        // must already be sorted
+        // sort on the insert.
+        match self
+            .data
+            .binary_search_by(|x| x.start_addr.cmp(&section.start_addr))
+        {
+            Err(s) => self.data.insert(s, section),
+            Ok(f) => (),
+        }
+    }
+
+    pub fn set_data(&mut self, addr: u32, data: u8) {
+        let section = self.find_section_mut(addr);
+        if section.is_some() {
+            section.unwrap().write_data(addr, data);
+        } else {
+            // quick test of neighboring data.
+            let mut skip_none = false;
+            if addr > 1 {
+                let sect2 = self.find_section_mut(addr - 1);
+                if sect2.is_some() {
+                    sect2.unwrap().push_data(data);
+                    skip_none = true;
+                }
+            }
+            if !skip_none {
+                let new_section = Section {
+                    start_addr: addr,
+                    data: vec![data],
+                };
+                self.insert_section(new_section);
+                // Two consolidates due to adding an entry into the middle of two sections.
+                self.consolidate();
+            }
+        }
+        self.consolidate();
+    }
+
+    pub fn write_data(&mut self, addr: u32, data: &Vec<u8>) {}
+
+    pub fn get_bytes(&self, addr: u32, size: usize) -> Option<Vec<u8>> {
+        match self.find_section(addr) {
+            Some(sec) => sec.read_data(addr, size),
+            None => None,
+        }
+    }
+}
 
 
 #[cfg(test)]
@@ -137,7 +255,14 @@ mod tests  {
     }
 
     #[test]
-    fn what_happens_on_test_failure() {
-	assert_eq!(1,2)
+    fn merge_order() {
+	let section_one = Section::new(0, vec![1,2,3,4,5]);
+	let section_two = Section::new(5, vec![6,7,8,9]);
+	let section_three = merge_sections(&section_one, &section_two);
+	let section_four = merge_sections(&section_two, &section_one);
+	assert!(section_three.is_some());
+	assert!(section_four.is_some());
+	assert_eq!(section_three.unwrap().data, section_four.unwrap().data);
     }
+
 }
